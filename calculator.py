bdafdff4091bd0bdab3191b07c0c141390e4ed3a
hao512991349 <hao512991349@qq.com>
2018-01-10 17:32:52 +0800

    mif

--- calculator.py
+++ calculator.py
@@ -1,9 +1,48 @@
 #!/usr/bin/env python3
-
 import sys
 
-counts = sys.argv[1:]
-def calculator(num_int):
+class Config(object):
+    def __init__(self, path):
+        self._config = {}
+        index = counts.index('-c')
+        path = counts[index+1]
+        with open(path) as f:
+            for i in f:
+                name, value = i.split('=')
+                self._config[name.strip()] = value.strip()
+    def get_config(self):
+        return self._config
+
+
+class UserData(object):
+    def __init__(self, path):
+        self._userdata = {}
+        with open(path) as f:
+            for i in f:
+                num, value = i.split(',')
+                self._userdata[num] = value.strip()
+        self.she = 0
+        self.ge = 0
+        self.gong = 0
+  
+    def get_userdata(self)
+        return self._userdata
+
+    def calculator(co, config):
+        l = float(config['JiShuL'])
+        H = float(config['JiShuH'])
+        YangLao = float(config['YangLao'])
+        YiLiao = float(config['YiLiao'])
+        ShiYe = float(config['ShiYe'])
+        GongShang = float(config['GongShang'])
+        ShengYu = float(config['ShengYu'])
+        GongJiJin = float(config['GongJiJin'])
+        value = float(co['value'])
+        if value <= 
+        
+        
+            
+def calculator(num_int, ca):
     if num_int <= 0:
         tax = 0
     elif num_int <= 1500:
@@ -32,3 +71,20 @@ for count in counts:
         print('{}:{:.2f}'.format(num_int,nev))
     except:
         print('Parameter Error')
+
+
+if __name__ == "__main__":
+    
+    counts = sys.argv[1:]
+    index = counts.index('-c')
+    path = counts[index+1]
+    index1 = counts.index('-d')
+    path1 = counts[index1+1]
+    index2 = counts.index('o')
+    path2 = counts[index2+1]
+    config = Config(path)
+    configs = config.get_config()
+    user = UserData(path1)
+    users = user.get_userdata()
+    user.calculator()
+    user.dumptofile(path2)
